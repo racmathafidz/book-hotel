36c7fc290c3e72e6b921b0abb5de922d20e36943
racmathafidz <rac.hafidz@gmail.com>
2020-12-03 11:27:37 +0700

    Add Responsive Navbar

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,43 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import Button from 'elements/Button'
 import BrandIcon from 'parts/IconText'
 
 export default function Header(props) {
+
+    //getting current path location
     const getNavLinkClass = path => {
         return props.location.pathname === path ? " active" : ""
     }
+
+    //for collapse button 
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
     return (
         <header>
             <div className="container">
                 <nav className="navbar navbar-expand-lg navbar-light">
-                    <BrandIcon/>                    
+                    <BrandIcon/>      
+
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>              
                 
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav ml-auto">
-                        <li className={`nav-item${getNavLinkClass("/")}`}>
-                            <Button className="nav-link" type="link" href="">
-                                Home
-                            </Button>
-                        </li>
-                        <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
-                            <Button className="nav-link" type="link" href="/browse-by">
-                                Browse By
-                            </Button>
-                        </li>
-                        <li className={`nav-item${getNavLinkClass("/stories")}`}>
-                            <Button className="nav-link" type="link" href="/stories">
-                                Stories
-                            </Button>
-                        </li>
-                        <li className={`nav-item${getNavLinkClass("/agents")}`}>
-                            <Button className="nav-link" type="link" href="/agents">
-                                Agents
-                            </Button>
-                        </li>
-                    </ul>
-                </div>
+                    <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                            <li className={`nav-item${getNavLinkClass("/")}`}>
+                                <Button className="nav-link" type="link" href="">
+                                    Home
+                                </Button>
+                            </li>
+                            <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                                <Button className="nav-link" type="link" href="/browse-by">
+                                    Browse By
+                                </Button>
+                            </li>
+                            <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                                <Button className="nav-link" type="link" href="/stories">
+                                    Stories
+                                </Button>
+                            </li>
+                            <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                                <Button className="nav-link" type="link" href="/agents">
+                                    Agents
+                                </Button>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
         </header>
