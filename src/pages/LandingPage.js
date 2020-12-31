1cf086d3738fe2a66752161702126ab11ce22ac4
racmathafidz <rac.hafidz@gmail.com>
2020-12-31 10:59:58 +0700

    Done with Footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,6 +7,7 @@ import Hero from 'parts/Hero'
 import MostPicked from 'parts/MostPicked'
 import Categories from 'parts/Categories'
 import Testimony from 'parts/Testimony'
+import Footer from 'parts/Footer'
 
 export default class LandingPage extends Component {
 
@@ -24,6 +25,7 @@ export default class LandingPage extends Component {
                 <MostPicked refMostPicked={this.refMostPicked} data={landingPageJson.mostPicked}/>
                 <Categories data={landingPageJson.categories}/>
                 <Testimony data={landingPageJson.testimonial}/>
+                <Footer/>
             </>
         )
     }
