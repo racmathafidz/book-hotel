7e8928c415f4aa834cf3cdfb8ac4f050787d474c
racmathafidz <rac.hafidz@gmail.com>
2021-01-10 12:46:05 +0700

    Done With Detail Page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 
 import "assets/scss/style.scss"
 import LandingPage from 'pages/LandingPage'
+import DetailPage from 'pages/DetailsPage'
+import Checkout from 'pages/Checkout'
+import Example from 'pages/Example'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/properties/:id" component={DetailPage}></Route>
+        <Route path="/checkout" component={Checkout}></Route>
+        <Route exact path="/example" component={Example}></Route>
       </Router>
     </div>
   );
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,12 +2,12 @@
 import React, { Component } from 'react'
 
 import Header from 'parts/Header'
-import landingPageJson from 'json/landingPage'
 import Hero from 'parts/Hero'
 import MostPicked from 'parts/MostPicked'
 import Categories from 'parts/Categories'
 import Testimony from 'parts/Testimony'
 import Footer from 'parts/Footer'
+import landingPageJson from 'json/landingPage'
 
 export default class LandingPage extends Component {
 
@@ -17,6 +17,11 @@ export default class LandingPage extends Component {
         this.refMostPicked = React.createRef();
     }
 
+    componentDidMount() {
+        window.title = "Details Page"
+        window.scrollTo(0, 0)
+    }
+    
     render() {
         return (
             <>
